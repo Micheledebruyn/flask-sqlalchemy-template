c1c2091edeeaf74cf9d21961019c6ab3823f1054
michele de bruyn <debruyn.mm@gmail.com>
2019-11-26 17:23:07 +0100

    fake first setup of a model class

---
+++ app/models.py
@@ -0,0 +1,9 @@
+from app import db
+
+# Example of usage of a random User model
+# class User(db.Model):
+#     __tablename__ = '<tablename>'
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String)
+#     email = db.Column(db.String)
+#     city_id = db.Column(db.Integer, db.ForeignKey('cities._id'))
