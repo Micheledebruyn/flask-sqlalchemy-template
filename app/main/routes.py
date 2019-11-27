2e3a266df577d556d3b0ec77c6dac39f7d374006
michele de bruyn <debruyn.mm@gmail.com>
2019-11-27 14:52:25 +0100

    add helpers folder structure

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,6 @@
-from app.models import User
 from . import main
-
+from app.models import User
+from app.helpers.api_helper import say_hello_again # Example of import from helpers
 
 @main.route('/')
 def say_hello():
